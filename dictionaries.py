fe8671348ae97f0210b112b2bff0edd2bd9748e0
Ezechiel <ezechiel.dosreis@gmail.com>
2019-08-15 19:00:06 -0400

    Organizing Functions
    Split up the code that was solely in scrape into two files: scrape (get_tweets function only) and set(creating the dictionary and generating tweets).

--- dictionaries.py
+++ dictionaries.py
@@ -1,11 +1,10 @@
 import tweepy
 
 from keys import consumer_key, consumer_secret, access_token, access_secret
-from scrape import auth, api, get_tweets, markov
+from scrape import auth, api, get_tweets
+from set import markov
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
-api = tweepy.API(auth)
 
 def dict_maker(handle):
     text = get_tweets(handle)
