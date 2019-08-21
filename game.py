6be592d638ed447033d23ef6ca4e065b674e5fee
Ezechiel <ezechiel.dosreis@gmail.com>
2019-08-21 14:35:46 -0400

    core generation mechanic implemented
    This draft of the game is nearly done.
    the launcher function now has real and fake tweets for each option; question and retrieve_tweet helper functions created; the "main piece" is done, so now I have to implement the scoring system, and win/loss state.

--- game.py
+++ game.py
@@ -1,26 +1,45 @@
-from dictionaries import lil_nas_x, mark_hamill, playstation
+import random
+
 from create import generate_text
-
-print ('Welcome to Twitwit! please select an option below.')
-
-print('1.) Lil Nas X')
-print('2.) Mark Hamill')
-print('3.) PlayStation')
+from dictionaries import lil_nas_x, mark_hamill, playstation
+from scrape import get_tweets
 
 def launcher(number):
     assert type(number) == 1 or 2 or 3
 
     if number == 1:
-        return generate_text(lil_nas_x, 20)
+        real = retrieve_tweet('LilNasX')
+        fake = generate_text(lil_nas_x, 10)
+        print(question(real, fake))
     elif number == 2:
-        return generate_text(mark_hamill, 20)
+        real = retrieve_tweet('HamillHimself')
+        fake = generate_text(mark_hamill, 10)
+        print(question(real, fake))
     elif number == 3:
-        return generate_text(playstation, 20)
+        real = retrieve_tweet('PlayStation')
+        fake = generate_text(playstation, 10)
+        print(question(real, fake))
+
+def retrieve_tweet(account):
+    statuses = get_tweets(account)
+    status_list = statuses.split('\n')
+    randomizer = random.choice(status_list)
+
+    return randomizer
+
+def question(t1, t2):
+        text = 'which one is real?' + '\n\n'
+        text += t1 + '\n\n' + 'or' + '\n\n' + t2
+
+        return text
+
+
+print ('Welcome to Twitwit! please select an option below.')
+
+print('1.) Lil Nas X' + '\n' + 
+    '2.) Mark Hamill' + '\n' +
+    '3.) PlayStation')
 
 account = int(input('choice: '))
 
-generated = launcher(account)
-
-print(generated)
-
-
+launcher(account)
